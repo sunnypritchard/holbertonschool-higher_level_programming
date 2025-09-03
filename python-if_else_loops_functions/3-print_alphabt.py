59e5438f9dfd869766f2f71278cd20fe6879b89d
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-03 10:03:59 +0200

    prints the alphabet in lowercase except e and q

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Print the alphabet in lowercase except e and q
+"""
+for letter in range(97, 123):
+    if chr(letter) == 'e' or chr(letter) == 'q':
+        continue
+    print("{}".format(chr(letter)), end="")
