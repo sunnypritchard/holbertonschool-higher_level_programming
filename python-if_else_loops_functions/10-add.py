58001125888a9c608a5ebdc7dd76517fcc5f6714
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-03 12:40:33 +0200

    adds two numbers

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Function:
+    add(a, b): Adds two numbers.
+
+Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+Returns:
+    int or float: The sum of a and b.
+"""
+
+
+def add(a, b):
+    return a + b
