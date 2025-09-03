51cd86bddea29eff2a58a9af533a37405c23a0ba
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-03 12:47:09 +0200

    computes the power of a number

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function:
+    pow(a, b): Computes the power of a number.
+
+Parameters:
+    a (int or float): The base.
+    b (int): The exponent.
+
+Returns:
+    int or float: The result of a raised to the power of b.
+"""
+
+
+def pow(a, b):
+    return a ** b
