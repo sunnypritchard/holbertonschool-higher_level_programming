25bd197a97a4847e8247c82cba18e02eb5788fd5
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-08 11:42:37 +0200

    replaces an element in a copy of a list at a specific index

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+Function:
+    new_in_list(my_list, idx, element): replaces an element in a copy of a list 
+    at a specific index
+
+Parameters:
+    my_list: list of integers
+    idx: index of the element to replace
+    element: new element to insert
+
+Returns:
+    new list with the updated element
+'''
+
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
