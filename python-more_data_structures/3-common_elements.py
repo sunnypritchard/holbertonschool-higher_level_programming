e5a9e60fb5d524ad287f23f851fd6d8fcfd3acee
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-09 17:52:27 +0200

    returns a set of common elements in two sets

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Function:
+    common_elements(set_1, set_2):
+        Returns a set of common elements in two sets.
+
+Parameters:
+    set_1: first set.
+    set_2: second set.
+
+Returns:
+    A set of common elements.
+'''
+
+
+def common_elements(set_1, set_2):
+    return set_1 & set_2
