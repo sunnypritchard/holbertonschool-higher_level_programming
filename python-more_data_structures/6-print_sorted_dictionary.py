e421e32a7ddd4a8620bf991411349469f5fe7db6
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-10 12:07:08 +0200

    refactor key indexing

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -13,5 +13,5 @@ Returns:
 
 
 def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary.keys()):
+    for key in sorted(a_dictionary):
         print("{}: {}".format(key, a_dictionary[key]), end="\n")
