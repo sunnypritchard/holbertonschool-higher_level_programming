0bc38dde34a7f445ae5ab2324e243b40c587f490
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-10 13:28:02 +0200

    refactor from list comprehension -> loop and condiction

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -15,4 +15,10 @@ Returns:
 
 
 def search_replace(my_list, search, replace):
-    return [replace if x == search else x for x in my_list]
+    new_list = []
+    for x in my_list:
+        if x == search:
+            new_list.append(replace)
+        else:
+            new_list.append(x)
+    return new_list
