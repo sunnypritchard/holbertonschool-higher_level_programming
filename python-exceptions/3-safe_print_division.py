2257a45a18af968cbc4607e981aeb201a81174ef
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-11 14:11:42 +0200

    perform a division and prints the result

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Function:
+    Performs a division and prints the result.
+
+Parameters:
+    a (int or float): The numerator.
+    b (int or float): The denominator.
+
+Returns:
+    float or None: The result of the division, or None if division by zero occurs.
+"""
+
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except (ZeroDivisionError):
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
+
+
+if __name__ == "__main__":
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
