740bfc300e128971bd290b5e6c6113d26d2bb0b5
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-11 13:18:51 +0200

    safely prints an integer

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Function:
+Safely prints an integer.
+
+Parameters:
+    value: The value to print.
+
+Returns:
+    bool: True if the value was printed, False otherwise.
+"""
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
+
+
+if __name__ == "__main__":
+    def test_safe_print_integer(value):
+        has_been_print = safe_print_integer(value)
+        if not has_been_print:
+            print("{} is not an integer".format(value))
+
+    value = 89
+    test_safe_print_integer(value)
+
+    value = -89
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = "School"
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
