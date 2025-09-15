5599439b7a60187d65c3d016d2dc11849f9cced8
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-15 09:22:15 +0200

    prints a square with '#' characters

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This is the "print_square" module
+
+This module supplies one function: print_square(). For example,
+
+>>> print_square(3)
+###
+###
+###
+"""
+
+
+def print_square(size):
+    """
+    This function prints a square with '#' character to represent it.
+        'size' must but an integer.
+        'size' is used to define the width and height of the square.
+    A TypeError is raised if size is not an integer.
+    A ValueError is raised if size is negative.
+    """
+    if not isinstance(size, int):
+        if isinstance(size, float) and size < 0:
+            raise TypeError("size must be an integer")
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for _ in range(size):
+        print("#" * size)
