d3c12e6d7941dc1c90fa18b152e9de61f47a711e
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-18 12:42:43 +0200

    fix: enforce by name mangling (_width -> __width) 'private attr'

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -16,8 +16,8 @@ class Rectangle:
 
     @property
     def width(self):
-        """Gets the width of the rectangle."""
-        return self._width
+        """Retrieves the width of the rectangle."""
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -34,12 +34,12 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
-        """Gets the height of the rectangle."""
-        return self._height
+        """Retrieves the height of the rectangle."""
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -56,4 +56,13 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print(my_rectangle.__dict__)
+
+    my_rectangle.width = 10
+    my_rectangle.height = 3
+    print(my_rectangle.__dict__)
