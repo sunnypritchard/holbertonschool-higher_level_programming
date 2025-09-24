759d348b46af69c415fe01f9449de3f2216d5c45
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-24 22:50:49 +0200

    feat(is_same_class): implement function to check exact class matches - Resturn True if obj is exactly an instance of a_class - Otherwise, return False

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+"""
+Module that defines a function to check if an object is exactly an instance
+of a specified class.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Return True if obj is exactly an instance of a_class.
+    Otherwise False.
+
+    param:
+        obj: The object to check.
+        a_class: The class to match the type of obj to.
+
+    """
+    return type(obj) is a_class
