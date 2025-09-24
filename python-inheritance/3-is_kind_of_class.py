68bd6fec195d815814585fa7e160d035f905b8c5
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-25 00:57:24 +0200

    feat(is_kind_of_class): returns True is an object is an instance of a class or instance of a subclass

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module that defines a function to check if an object is an instance of a
+specified class or a subclass thereof.
+"""
+
 
 def is_kind_of_class(obj, a_class):
     """
