ce59649c710bb93aca35ceb7178f40b2429fbfb9
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-24 02:53:53 +0200

    feat(lookup(obj)): returns a list of available attributes and methods of an object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -11,8 +11,5 @@ def lookup(obj):
 
     param:
         obj: The object to inspect.
-
-    return:
-        A list of strings representing the names of the attributes and methods.
     """
     return dir(obj)
