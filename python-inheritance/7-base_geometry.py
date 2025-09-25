9bd694918240511ed3571857d4f145b98f8deda5
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-26 01:23:39 +0200

    fix(base_geometry) enfore that the value pass to the method integer_validator must be a plain integer

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -21,7 +21,7 @@ class BaseGeometry:
             TypeError: must be an integer.
             ValueError: must be greater than 0.
         """
-        if type(value) is not int:
+        if isinstance(value, bool) or not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
