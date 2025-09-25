fe6a630fb374041838d6a7196ca42021e790aac9
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-26 01:56:43 +0200

    feat(Rectangle): define the class, and instante with private attributes

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,31 +1,8 @@
 #!/usr/bin/python3
 """
-Module that defines a class BaseGeometry with an unimplemented area method.
+Module that defines a Rectangle class that inherits from BaseGeometry.
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class BaseGeometry:
-    """A class representing base geometry."""
-
-    def area(self):
-        """Raise an exception because area is not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate that a value is a positive integer.
-
-        Args:
-            name (str): The name of the variable being validated.
-            value (int): The value to validate.
-        Raises:
-            TypeError: must be an integer.
-            ValueError: must be greater than 0.
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
