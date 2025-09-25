112116476e1dc495c18210b5ae5d4c6a36bbad2a
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-25 13:33:20 +0200

    feat(inherits_from) checks if an object is derived from a_class, excluding instances of the class itself

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
 Module that defines a function to check if an object is an instance of a
-class that inherited (directly or indirectly) from the specified class.
+specified class, excluding instances of the class itself.
 """
 
 
 def inherits_from(obj, a_class):
     """
-    Return True if obj is an instance of a class that inherited (directly or
-    indirectly) from a_class; otherwise False.
+    Return True if obj is an instance of a subclass of a_class or an inheritance
+    of a_class; otherwise False.
 
     param:
         obj: The object to check.
