f1c3924b8624bfa73fe04eaf5c311aca6f9150ef
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-26 02:57:04 +0200

    feat(Sqaure): define a Square class that inherits from Rectangle class

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,7 +7,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """A class representing a square, inheriting from Rectangle."""
     def __init__(self, size):
+        """Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square's sides.
+        """
         self.integer_validator("size", size)
         self.__size = size
 
