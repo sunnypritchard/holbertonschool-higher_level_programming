58b67c3fcbcc66b9867e8b6f1a498dd1c045e944
Sunny Pritchard <pritchardsunny@gmail.com>
2025-10-10 12:43:47 +0200

    style(task 04) pycodestyle comments

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-This modules defines a simple API using Flask,
-with multiples methods to access different routes
+This module defines a simple API using Flask,
+with multiple methods to access different routes
 and handle a POST request.
 """
 from flask import Flask, jsonify, request
-
 
 app = Flask(__name__)
 users = {}
@@ -25,7 +24,7 @@ def home():
 @app.route("/data")
 def jsonify_data():
     """
-    This methods allows to access the 'data' route containing users data.
+    This method allows access to the 'data' route containing users data.
 
     Returns:
         A JSON containing the list of all users.
@@ -36,7 +35,7 @@ def jsonify_data():
 @app.route("/status")
 def status():
     """
-    This method returns the status OK
+    This method returns the status OK.
     """
     return "OK"
 
@@ -44,14 +43,14 @@ def status():
 @app.route("/users/<string:username>")
 def dynamic_route(username):
     """
-    This methods allows dynamic routing when trying
-    to access a specific user data.
+    This method allows dynamic routing when trying
+    to access a specific user’s data.
 
     Arg:
-        username: the name of the user for the route access.
+        username: The name of the user for the route access.
     Returns:
-        The JSON user data if User exist.
-        Else return 'error': 'User not found'.
+        The JSON user data if the user exists.
+        Else returns 'error': 'User not found'.
     """
     if username in users:
         response = jsonify(users[username])
@@ -64,21 +63,32 @@ def dynamic_route(username):
 @app.route("/add_user", methods=["POST"])
 def add_user():
     """
-    This method allows to make a POST request to add a new User,
+    This method allows a POST request to add a new user
     using JSON data.
 
+    Expected JSON format:
+        {
+            "username": "name",
+            "data": {"key": "value"}
+        }
+
     Returns:
-        If a username is provided, returns confirmation message
-        with the User data and status code 201.
-        Else return 'error': 'Username is required'.
+        If a username is provided and valid, returns a confirmation message
+        with the user data and status code 201.
+        Else returns 'error': 'Username is required' with status code 400.
     """
     user_data = request.get_json()
-    if 'username' not in user_data:
+
+    if not user_data or "username" not in user_data:
         return jsonify({"error": "Username is required"}), 400
 
     username = user_data["username"]
-    users[username] = user_data.copy()
-    return jsonify({"message": "User added", "user": users[username]}), 201
+    users[username] = user_data.get("data", {})
+
+    return jsonify({
+        "message": "User added",
+        "user": {username: users[username]}
+    }), 201
 
 
 if __name__ == "__main__":
