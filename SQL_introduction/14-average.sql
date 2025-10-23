2673aff7dc643999eaaa0610ae32555cb2c43742
Sunny Pritchard <pritchardsunny@gmail.com>
2025-10-23 09:57:04 +0200

    docs((SQL introduction) add commits

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,1 +1,2 @@
+-- This SQL query calculates the average score from the second_table
 SELECT SUM(score) / COUNT(score) AS average FROM second_table;
--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,6 @@
+-- This SQL query selects the score and name columns from the second_table
+-- It filters out any rows where the name is NULL
+-- Finally, it orders the results by score in descending order
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL
