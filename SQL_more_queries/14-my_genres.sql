a2dc166415f02c75a1cb221700c3caa9df3d3b84
Sunny Pritchard <pritchardsunny@gmail.com>
2025-10-23 14:37:58 +0200

    feat(SQL more queries): add genre aggregation and show-specific genre queries
    Add two new SQL queries for working with TV show and genre relationships:
    - Query to count and display shows per genre with JOIN and GROUP BY
    - Query to retrieve all genres for a specific TV show using multiple JOINs

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Retrieve the names of all genres associated with the TV show titled 'Dexter'
+-- The results are ordered by genre name in ascending order
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
