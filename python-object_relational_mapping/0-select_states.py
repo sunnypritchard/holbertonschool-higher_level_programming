5351150fe5c645f55b0b8d677a8520b72392c2df
Sunny Pritchard <pritchardsunny@gmail.com>
2025-10-25 16:37:12 +0200

    fix(states) add utf8 encoding to database connection parameter)

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module task 0"""
+"""Module that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
@@ -10,7 +10,8 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        charset="utf8"
     )
 
     cursor = conn.cursor()
