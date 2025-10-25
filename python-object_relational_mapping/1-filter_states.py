2d5d9f0a5e321e94c98956456f874d5c3885fc2f
Sunny Pritchard <pritchardsunny@gmail.com>
2025-10-25 17:01:08 +0200

    fix: Enforce case-sensitive filtering and UTF-8 charset

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,6 +5,7 @@ from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
     conn = MySQLdb.connect(
         host="localhost",
@@ -16,7 +17,8 @@ if __name__ == "__main__":
     )
 
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' "
+                   "ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
