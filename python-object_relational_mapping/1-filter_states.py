6fe570ab6eba3d80f819f42a25e6857337dc8104
Sunny Pritchard <pritchardsunny@gmail.com>
2025-10-25 16:33:45 +0200

    feat(states): filter states by name starting with N
    - add script to query and display states with names starting with 'N'
    - from the MySQL database, sorted in ascending order by states.id

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module that lists all states with the name starting with 'N'
+from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
