70cd349abd644f450fa4930ac89bad9c5d7bf323
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-06 22:57:03 +0100

    compute and prints a factorial of x first argv

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+/*
+This script computes and prints a factorial recursively.
+- The first argument is the integer to compute the factorial of.
+- Factorial of NaN is 1.
+- Uses recursion to calculate the result.
+*/
+
+function factorial (n) {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(process.argv[2], 10);
+console.log(factorial(num));
