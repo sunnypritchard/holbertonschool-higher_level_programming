38bfe7ff40a090c401c275c1d13159997e387587
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-06 15:38:59 +0100

    prints three lines of string

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+/*
+ * This script prints three lines:
+ * - C is fun
+ * - Python is cool
+ * - JavaScript is amazing
+ */
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
