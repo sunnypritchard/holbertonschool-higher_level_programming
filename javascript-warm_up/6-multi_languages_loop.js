cb278e58dbbf23bd8dcba430f6fab76fd3c794bb
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-06 21:46:12 +0100

    fix: typo in string naming

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -7,7 +7,7 @@ This script prints three lines of strings using an array and a loop:
 - JavaScript is amazing
 */
 
-const languages = ['C is fun', 'Pyhton is cool', 'JavaScritpt is amazing'];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < languages.length; i++) {
   console.log(languages[i]);
