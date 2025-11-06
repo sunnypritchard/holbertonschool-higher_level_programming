edd9a29c730a27bacc0e2196af9b885feabc5fe8
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-06 17:26:42 +0100

    style: implement standard js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 /*
 This script prints a message depending on the number of arguments passed:
@@ -6,12 +6,12 @@ This script prints a message depending on the number of arguments passed:
 - If one argument is passed, it prints "Argument found"
 - If two or more arguments are passed, it prints "Arguments found"
 */
-const argCount = process.argv.length;
+const argCount = process.argv.length - 2;
 
-if (argCount === 2) {
-    console.log('No argument');
-} else if (argCount === 3) {
-    console.log('Argument found');
+if (argCount === 0) {
+  console.log('No argument');
+} else if (argCount === 1) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
