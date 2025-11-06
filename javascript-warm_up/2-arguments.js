373365bca07fb2de0e98728b5441ef5ae78c567c
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-06 16:35:40 +0100

    feat: prints a message base on the number of arguments passed

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+/*
+This script prints a message depending on the number of arguments passed:
+- If no arguments are passed, it prints "No argument"
+- If one argument is passed, it prints "Argument found"
+- If two or more arguments are passed, it prints "Arguments found"
+*/
+const argCount = process.argv.length - 2;
+
+if (argCount === 0) {
+    console.log('No argument');
+} else if (argCount === 1) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
