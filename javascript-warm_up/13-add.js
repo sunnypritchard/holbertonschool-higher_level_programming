8f45211e73e26510268c08e7ee56b8b8d1e854c0
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-07 01:40:22 +0100

    module that return the sum of two integers

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/*
+This module exports a function that adds two numbers.
+The function takes two arguments and returns their sum.
+*/
+exports.add = function (a, b) {
+  return a + b;
+};
