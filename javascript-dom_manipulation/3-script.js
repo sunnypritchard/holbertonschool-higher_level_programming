cc48b6fb7d52988279c21570720fea9c8d1d63bb
Sunny Pritchard <pritchardsunny@gmail.com>
2025-11-10 12:04:59 +0100

    toggles the class of the header element

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,21 @@
+/*
+This script toggles ('red' || 'green') the class of the header element
+when user clicks on the element with id 'toggle_header'.
+*/
+
+function toggleClass (element, className1, className2) {
+  if (element.classList.contains(className1)) {
+    element.classList.replace(className1, className2);
+  } else {
+    element.classList.replace(className2, className1);
+  }
+}
+
+const toggleHeader = document.querySelector('#toggle_header');
+const header = document.querySelector('header');
+
+toggleHeader.addEventListener('click', function () {
+  toggleClass(header, 'red', 'green');
+});
+
+console.log('Script loaded: toggleClass on header on click');
